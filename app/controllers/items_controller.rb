d6e85a98e81d5d126ac081bc0d724446ecc87bff
KEYproducts <keiko.yamamoto51@gmail.com>
2023-05-27 14:45:11 +0900

    商品出品ページへのリンク作成
    ログイン状態：商品出品ページへ遷移
    ログアウト状態：ログインページへリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :confilm_user_signed_in, only: :new
+
   def index
   end
 
@@ -7,5 +9,13 @@ class ItemsController < ApplicationController
   end
 
   def create
+  end
+
+  private
+
+  def confilm_user_signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
