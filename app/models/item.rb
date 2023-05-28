1f13d3586f6768e08efaf4b7d0909ace728dba09
KEYproducts <keiko.yamamoto51@gmail.com>
2023-05-28 15:03:24 +0900

    バリデーションの設定
    binding.pryの削除漏れも修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,15 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :scheduled_delivery
-  
+
+  validates :image, :name, :text,
+            presence: true
+  validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, 
+            numericality: { other_than: 1 , message: "can't be blank" }
+  validates :price,
+            presence: true,
+            numericality: { other_than: 0 , message: 'is invalid. Input half-width characters' },
+            inclusion: { in: 300..9999999, message: 'is out of setting range' }
+
 end
+
