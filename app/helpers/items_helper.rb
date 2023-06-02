787d7138b18022b4662d0304c0ddf63c8fdc8a19
KEYproducts <keiko.yamamoto51@gmail.com>
2023-06-02 14:50:14 +0900

    売り切れ商品に関する表示の設定
    ・「sold out」の表示
    ・編集、削除、購入ボタンの非表示

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,17 @@
 module ItemsHelper
+  def is_purchased?(item)
+    if item.order != nil
+      return true
+    else
+      return false
+    end
+  end
+
+  def is_contributor?(item)
+    if current_user.id == item.user.id
+      return true
+    else
+      return false
+    end
+  end
 end
